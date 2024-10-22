68ae402aa278df9719613cd82789c6ba6721106f
Agungpz <142970463+Agungpz@users.noreply.github.com>
2024-10-22 13:59:05 +0700

    Add files via upload

---
+++ bukukita/peminjamancontroller.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'Database.php';
+require_once 'peminjaman.php';
+
+class PeminjamanController {
+    private $peminjaman;
+
+    public function __construct() {
+        $database = new Database();
+        $db = $database->getConnection();
+        $this->peminjaman = new Peminjaman($db);
+    }
+
+    // Fetch all borrowings
+    public function getAllPeminjaman() {
+        return $this->peminjaman->getAllPeminjaman();
+    }
+
+    public function getPeminjamanToday() {
+        return $this->peminjaman->getPeminjamanToday();
+    }
+    
+
+    // Update borrowing status
+    public function updateStatus($id, $status, $tanggal_kembalian) {
+        // Gunakan instance $this->peminjaman yang sudah ada
+        return $this->peminjaman->updateStatus($id, $status, $tanggal_kembalian);
+    }
+
+    // Delete borrowing
+    public function deletePeminjaman($id) {
+        return $this->peminjaman->deletePeminjaman($id);
+    }
+}
+?>
